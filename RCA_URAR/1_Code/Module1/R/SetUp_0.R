01a13e5df2da8da9ce2c77ceb6846dcfc4152813
William Bert Craytor <69237297+bert-craytor@users.noreply.github.com>
2023-03-17 11:41:15 -0700

    Update SetUp_0.R
    Updated license
    
    Signed-off-by: William Bert Craytor <69237297+bert-craytor@users.noreply.github.com>

--- RCA_URAR/1_Code/Module1/R/SetUp_0.R
+++ RCA_URAR/1_Code/Module1/R/SetUp_0.R
@@ -1,13 +1,27 @@
-################################################   Setup #########################################
-# Author:       Wm. Bert Craytor
-# Location:     242 Clifton Rd., Pacifica, CA 94044, USA
-# License:      MIT License
-# Description:  Setup script to generate MARS (earth)  analysis for appraisal data using R:earth 
- 
+########################   Setup #############################
+#' Author:       Wm. Bert Craytor
+#' Location:     242 Clifton Rd., Pacifica, CA 94044, USA
+#' License:      MIT License
+#' Description:  Setup script to generate MARS (earth)  analysis for appraisal data using R:earth 
+#'
+#' Notes:        1.  This program is free software; you can redistribute it and/or modify
+#'                   it under the terms of the GNU General Public License as published by
+#'                   the Free Software Foundation; either version 3 of the License, or
+#'                   (at your option) any later version.
+#'
+#'               2.  This program is distributed in the hope that it will be useful,
+#'                   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#'                   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#'                   GNU General Public License for more details.
+#'
+#'               3.   A copy of the GNU General Public License is available at
+#'                   http://www.r-project.org/Licenses
+#' Purpose:
+#'
 #'  Loads the initial MLS Excel Data File to Data Frames
 #'  @return  Project Environment
 #'
-#' #export
+##############################################################
 LoadMlsExcelToDataFrame <- function() {
 flog.info("Stage I Finished",name="Log2File")
 
